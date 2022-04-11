4d0f776e50e5d4e244c2591f3450133eb09b838e
Igor284 <kochat.lgor@gmail.com>
2022-04-11 23:36:09 +0300

    bad commit

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,12 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'; 
 import { FileUploader } from 'ng2-file-upload';
 import { take } from 'rxjs/operators';
 import { Member } from 'src/app/_models/member';
 import { User } from 'src/app/_models/user';
 import { AccountService } from 'src/app/_services/account.service';
-import { MembersService } from 'src/app/_services/members.service';
 import { environment } from 'src/environments/environment';
-
+ 
 @Component({
   selector: 'app-photo-editor',
   templateUrl: './photo-editor.component.html',
@@ -14,38 +13,38 @@ import { environment } from 'src/environments/environment';
 })
 export class PhotoEditorComponent implements OnInit {
   @Input() member: Member;
-  uploader: FileUploader;
+  uploader : FileUploader;
   hasBaseDropzoneOver = false;
   baseUrl = environment.apiUrl;
-  user: User;
-
-  constructor(private accountService: AccountService, private memberService: MembersService) { 
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user == user);
-  }
-  
+  user : User;
+ 
+  constructor(private accountService: AccountService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user=user)
+   }
+ 
   ngOnInit(): void {
-    this.initializeUploader()
+    this.initializeUploader();
   }
-
-  fileOverBase(e: any) {
+ 
+  fileOverBase(e: any){
     this.hasBaseDropzoneOver = e;
   }
-
+ 
   initializeUploader() {
     this.uploader = new FileUploader({
-      url: this.baseUrl + 'users/add-photo',
-      authToken: 'Bearer ' + this.user.token,
+      url: this.baseUrl + 'users/add-photo' ,
+      authToken: 'Bearer ' + this.user.token ,
       isHTML5: true,
       allowedFileType: ['Image'],
       removeAfterUpload: true,
       autoUpload: false,
       maxFileSize: 10 * 1024 * 1024
     });
-
+ 
     this.uploader.onAfterAddingFile = (file) => {
       file.withCredentials = false;
     }
-
+ 
     this.uploader.onSuccessItem = (item, response, status, headers) => {
       if(response) {
         const photo = JSON.parse(response);
@@ -55,3 +54,4 @@ export class PhotoEditorComponent implements OnInit {
   }
 
 }
+
